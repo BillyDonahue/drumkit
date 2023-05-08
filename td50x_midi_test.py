02327a2c0591035bb75ccff397d8612c78068526
Billy Donahue <billy.donahue@gmail.com>
2023-05-08 03:27:04 -0400

    poll

--- td50x_midi_test.py
+++ td50x_midi_test.py
@@ -38,6 +38,12 @@ def flatten(*args):
     return out
 
 
+def unpack4(arr):
+    n = 0
+    for x in arr:
+        n = (n << 7) + x
+    return n
+
 def pack4(n):
     out = []
     for i in range(4):
@@ -53,7 +59,7 @@ def checksum(arr):
     return 128 - (sum % 128)
 
 
-def prepare_sysex_msg(addr, size):
+def prepare_sysex_msg(addr:int, size:int):
     """add the status fields and checksum to the message"""
     msg = flatten(
         _STATUS_SYSEX, _VENDOR_ID_ROLAND, _DEVICE_ID, _MODEL_TD50X, _COMMAND_RQ1
@@ -96,15 +102,42 @@ def find_devices():
     return input_device_id, output_device_id
 
 
-def parse_sysex(kit, buf):
-    s = bytes(b & 0x7f for b in buf).decode(encoding='ascii')
-    # print(buf)
-    s = "".join(s[13:-4])
+def parse_sysex(buf) -> int:
+    ok = True
+    ok = ok or buf[0] == 0xf0
+    ok = ok or buf[1] == 0x41
+    dev = buf[2] 
+    ok = ok or buf[3:8] == [0,0,0,0,7]
+    ok = ok or buf[8] == 0x12
+    addr = buf[9:13]
+    data = buf[13:-2]
+    sum = buf[-2]
+    ok = ok or buf[-1] == 0xf7
+    if not ok:
+        return None
+    addr = unpack4(addr)
+    # s = "".join(s[13:-4])
     # s.decode(encoding="ascii")
     # printSync(f'{[f"{b:02x}" for b in buf]}')
-    printSync(f"{kit+1:03d} : '{s[0:12]:12}' '{s[12:]}'")
-    with open('kit.txt', 'w') as f:
-        print(f"{kit+1:03d} : {s[0:12]:12}\n{s[12:]}", file=f)
+
+    kit_offset = 0
+    kit_name_start = 4 << 21
+    kit_step = 2 << 14
+    if addr == kit_offset:
+        kit = int(data[0]) + 1
+        printSync(f"Current kit: {kit:03d}")
+        return kit
+    if addr > kit_name_start:
+        kit = int((addr - kit_name_start) / kit_step)
+        name = bytes(b & 0x7f for b in data[0:12]).decode(encoding='ascii')
+        sub = bytes(b & 0x7f for b in data[12:12+15]).decode(encoding='ascii')
+        name = name.rstrip(' ')
+        sub = sub.rstrip(' ')
+        printSync(f"{kit+1:03d}:[name:12]/[{sub}]")
+        with open('kit.txt', 'w') as f:
+            print(f"{kit+1:03d}:{name:12}\n{sub}", file=f)
+        return None
+    return None
 
 
 # Initialize Midi
@@ -115,47 +148,54 @@ printSync(f"Devices found: in=[{devices[0]}], out=[{devices[1]}]")
 midi_input = pygame.midi.Input(devices[0])
 midi_output = pygame.midi.Output(devices[1])
 
-# current_kit_sysex_msg = prepare_sysex_msg(0, 1)
-
 try:
     pending = None
-    kit_init_index = 0
+    kit_init_index = 1
     sysex_response_buffer = None
     sent = False
 
+    t_current_kit = time.time()
+
     while True:
-        if pending is None and kit_init_index < 100:
+        if pending is None and kit_init_index <= 100:
             #printSync(f"kit_init_index:{kit_init_index}")
             pending = kit_init_index
             kit_init_index += 1
 
         if pending is not None and not sent:
             #printSync(f"pending:{pending}")
-            kit_addr = (4 << 21) + pending * (2 << 14)
+            kit_addr = (4 << 21) + (pending - 1) * (2 << 14)
             msg = prepare_sysex_msg(kit_addr, 27)
             midi_output.write_sys_ex(0, msg)
             sent = True
+
+        now = time.time()
+        if t_current_kit is None or now - t_current_kit > 0.5:
+            t_current_kit = now
+            msg = prepare_sysex_msg(0, 1)
+            midi_output.write_sys_ex(0, msg)
 
         for event in pygame.midi.Input.read(midi_input, 16):
             data, timestamp = event
             if sysex_response_buffer is not None:
                 sysex_response_buffer.extend(data)
                 if _STATUS_EOX in data:
-                    parse_sysex(pending, sysex_response_buffer)
+                    pending = parse_sysex(sysex_response_buffer)
                     sysex_response_buffer = None
-                    pending = None
-                    sent = False
+                    if pending is not None:
+                        sent = False
             elif data[0] == _STATUS_SYSEX:
                 sysex_response_buffer = data
             elif data[0] == _STATUS_TIMING_CLOCK:
                 continue  # clock sync message
             elif data[0] == _STATUS_PROGRAM_CHANGE:
-                printSync(f'Kit changed to {data[1]+1:02d}')
+                # printSync(f'Kit changed to {data[1]+1:02d}')
                 if pending is None:
-                    pending = data[1]
+                    pending = data[1] + 1
             else:
                 #printSync(f'{[f"{d:02x}" for d in data]} {timestamp * 1e-3 : .3f}')
                 pass
+        time.sleep(0.001)
 
 except KeyboardInterrupt:
     midi_output.close()
